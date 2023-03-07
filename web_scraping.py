092a539cd99e30710f6f92bad2a3bb2f50de2264
DatProJack <alex.tang.yang@gmail.com>
2023-03-07 17:16:00 -0600

    infobox

--- web_scraping.py
+++ web_scraping.py
@@ -22,54 +22,81 @@ def clean(s):#cleans the string to remove characters between [] ()
             ret += i
     return ret
 
-url = "https://en.wikipedia.org/wiki/Wednesday_(TV_series)"#insert any url here-- only works for wikipedia tv series urls
 
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
+def parse_file(url, destination):
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
 
-titleparse = doc.find('title')
-s = str(titleparse.text)
-result = re.split(r"[-(]\s*", s)
-title = result[0].strip()
+    titleparse = doc.find('title')
+    s = str(titleparse.text)
+    result = re.split(r"[-(]\s*", s)
+    title = result[0].strip()
+    infobox = doc.find_all("th", {"class": "infobox-label"})
+    parsed_infobox = []
+    for element in infobox:
+        data = element.find_next()
+        content = str(data.text).strip()
+        content = content.replace("\n", "\",\" ")
+        parsed_infobox.append(str(element.text) + ": " + content)
+
+    headers = doc.find_all("h2")
+    #charactertypes = doc.find_all("h3")
+
+    bulletlist = None
+    for tag in headers:
+        if(tag.get_text().find('Cast')>=0):
+            bulletlist = tag
+            break
+        
+    file = open('./parsed_csv_files/' + destination + '.csv', 'a', newline='', encoding='utf-8')
+    writer = csv.writer(file)
 
 
-headers = doc.find_all("h2")
-charactertypes = doc.find_all("h3")
-
-for tag in headers:
-    if(tag.get_text().find('Cast')>=0):
-        bulletlist = tag
-        break
-
-scraped_sentences = []
-bulletlist = bulletlist.find_next()
-
-file = open('parsedtext.csv', 'w', newline='', encoding='utf-8')
-writer = csv.writer(file)
-
-i=0
-iteration = 1
-while bulletlist.name!="h2":
-    if bulletlist.name=="ul" or bulletlist.name=="td":#works better for bullet points, not so great for tables
-        if(bulletlist.sourceline > charactertypes[i+1].sourceline):#for tables it should be easier...
-            i = i + 1
-        s = str(charactertypes[i].text)
-        chartype = re.split(r"[-(,\[*]\s*", s)
-        chartype = chartype[0].strip()
-        for tag in bulletlist:
-            s = str(tag.text)
-            s = clean(s).strip() #cleaning up and removing whitespace
-            #from s: split at the first as and first comma
-            if(len(s)!=0):#skips over if the string is empty
-                s = s.split(" as ", maxsplit = 1)
-                actorname = clean(s[0])
-                if(len(s)==1):
-                    continue
-                s = s[1].split(",", maxsplit = 1)
-                charname = clean(s[0])
-                description = ""
-                if(len(s)>1):
-                    description = clean(s[1])
-                writer.writerow([title,chartype,iteration,actorname,charname,description])#write here
-                iteration = iteration + 1
+    if(bulletlist is None):
+        writer.writerow([title, '404 ERROR'])
+        return
     bulletlist = bulletlist.find_next()
+
+    firstrow = [title,"1"]
+    firstrow.extend(parsed_infobox)
+    writer.writerow(firstrow)
+
+
+    iteration = 2
+    while bulletlist.name!="h2":
+        if bulletlist.name=="ul" or bulletlist.name=="td":#works better for bullet points, not so great for tables
+            #if(i < len(charactertypes) - 1 and bulletlist.sourceline > charactertypes[i+1].sourceline):#for tables it should be easier...
+            #    i = i + 1
+            s = "Cast"
+            #if(len(charactertypes) != 0):
+            #    s = str(charactertypes[i].text)
+            #chartype = re.split(r"[-(,\[*]\s*", s)
+            #chartype = chartype[0].strip()
+            for tag in bulletlist:
+                s = str(tag.text)
+                s = clean(s).strip() #cleaning up and removing whitespace
+                #from s: split at the first as and first comma
+                if(len(s)!=0):#skips over if the string is empty
+                    s = s.split(" as ", maxsplit = 1)
+                    actorname = clean(s[0])
+                    if(len(s)==1):
+                        continue
+                    s = s[1].split(",", maxsplit = 1)
+                    charname = clean(s[0])
+                    description = ""
+                    if(len(s)>1):
+                        description = clean(s[1])
+                    writer.writerow([title,iteration,actorname,charname,description])#write here
+                    iteration = iteration + 1
+        bulletlist = bulletlist.find_next()
+
+
+
+
+file = open('urls.csv', 'r')
+i = 1
+
+for line in file.readlines():
+    print(i)
+    i = i + 1
+    parse_file(line.strip(), 'product')
